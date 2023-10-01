ef45b2c843616192ce19ad7f351d815e0df7add0
badr <bernardoantoniod@gmail.com>
2023-10-01 19:49:08 -0300

    enrutamiento

--- Ecommerce/src/Pages/App/index.jsx
+++ Ecommerce/src/Pages/App/index.jsx
@@ -1,22 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
 import Home from '../Home/index';
 import MyAccount from '../MyAccount/index';
 import MyOrders from '../MyOrders/index';
 import MyOrder from '../MyOrder';
-import NotFound from '../NotFound/index';
 import SignIn from '../SignIn/index';
+import NotFound from '../NotFound/index';
 import './App.css';
+
+const AppRoutes = () => {
+  let routes = useRoutes ([
+    { path: '/', element:<Home /> },
+    { path:'myaccount', element:<MyAccount /> },
+    { path:'myorders', element: <MyOrders />},
+    { path: 'myorder', element: <MyOrder /> },
+    { path: 'signin', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }
+  ])
+  return routes;
+}
 
 function App() {
 
   return (
-    <div className="bg-red-100">
-      <Home />
-      <MyAccount />
-      <MyOrder/>
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
