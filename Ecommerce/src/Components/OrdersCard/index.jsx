83ca14455949ef22856d299eaf1bdeca8500eb48
badr <bernardoantoniod@gmail.com>
2023-10-15 22:05:26 -0300

    styles to My Orders cards

--- Ecommerce/src/Components/OrdersCard/index.jsx
+++ Ecommerce/src/Components/OrdersCard/index.jsx
@@ -1,4 +1,4 @@
-import { XCircleIcon } from "@heroicons/react/24/outline"
+import { ChevronRightIcon } from "@heroicons/react/24/outline"
 
 
 const OrdersCard = props => {
@@ -6,12 +6,17 @@ const OrdersCard = props => {
     const { totalPrice, totalProducts } = props
 
     return (
-        <div className="flex justify-between items-center mb-3 border border-black">
-            <p>
-                <span>10.10.23</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
-            </p>
+        <div className="flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80 mb-4">
+            <div className="flex justify-between w-full">
+                <p className="flex flex-col">
+                    <span className="font-light"> Date: 10.10.23</span>
+                    <span className="font-light">Items: {totalProducts}</span>
+                </p>
+                <p className="flex items-center gap-2">
+                    <span className="font-medium text-2xl">$ {totalPrice}</span>
+                    <ChevronRightIcon className="h-6 w-6 text-black cursor-pointer" />
+                </p>
+            </div>
         </div>
     )
 }
